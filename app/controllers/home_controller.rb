01228545ec61798618a9f0e46f460afbeee0f023
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-09-02 22:27:32 -0400

    Add model and controller functions for uploading resume

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    render :file => 'app/views/resume/uploadfile.rhtml'
+  end
+
+  def uploadFile
+    post = DataFile.save(params[:upload])
+    render :text => "Your resume has been uploaded successfully. We'll be in touch. Good luck!"
   end
 end
